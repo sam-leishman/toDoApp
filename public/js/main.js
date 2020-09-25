8c0f84162039f51aadbad4ff9f120003b8580c9b
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2020-09-25 08:27:14 -0600

    Add color scheme and example js

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+// ====================
+// Make list from array
+// ====================
+
+// let fruits = ["🍉", "🍌", "🍓", "🍎", "🍍", "🍋", "🍇"];
+// let fruitName = ["Watermelon", "Banana", "Strawberry", "Apple", "Pineapple", "Lemon", "Grape"];
+// let stuff = "<ul>";
+
+// for (let i = 0; i < fruits.length; i++) {
+//     stuff += "<li>" + fruits[i] + " " + fruitName[i] + "</li>";
+// }
+
+// stuff += "</ul>"
+
+// let fruitList = document.getElementById("fruit-list");
+// fruitList.innerHTML = stuff;
