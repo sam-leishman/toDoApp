f954a4a999dc82e9c5732991e6a5328d699283cf
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2020-10-16 17:45:57 -0600

    Rewrite some JavaScript

--- public/js/main.js
+++ public/js/main.js
@@ -19,6 +19,7 @@ document.getElementById("month").innerText = monthName;
 
 
 
+/* OLD JAVASCRIPT
 // =========================
 // Create ID
 function getNewId() {
@@ -44,6 +45,7 @@ function printItem() {
         `;
     }
     */
+   /*
     currentList.listItems.forEach(toDoItem => {
         listHtml += `
         <div id="${toDoItem.id}">
@@ -146,3 +148,43 @@ function selectList(listId) {
     printItem();
     printLists();
 }
+*/
+
+const listsContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+
+let lists = [];
+
+newListForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const listName = newListInput.nodeValue;
+    if (listName == null || listName === '') return
+    const list = createList(listName)
+    newListInput.value = null;
+    lists.push(list)
+    render();
+})
+
+function createList(name) {
+    return { id: Date.now().toString(), name: name, tasks: [] }
+}
+
+function render() {
+    clearElement(listsContainer)
+    lists.forEach(list => {
+        const listElement = document.createElement('li')
+        listElement.dataset.listId = list.id
+        listElement.classList.add('list-name')
+        listElement.innerText = list.name;
+        listsContainer.appendChild(listElement)
+    })
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+render();
